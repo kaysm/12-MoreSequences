152a6be91a06f1531804363b65db10a3aef863a6
kaysm <kaysm@rose-hulman.edu>
2019-03-18 15:05:50 -0400

    l

--- src/m4_accumulating_sequences.py
+++ src/m4_accumulating_sequences.py
@@ -14,17 +14,17 @@ import rosegraphics as rg
 
 def main():
     """ Calls the various   TEST   functions in this module. """
-    # run_test_make_simple_list()
-    # run_test_make_simple_string()
-    # run_test_make_less_simple_string()
+    run_test_make_simple_list()
+    run_test_make_simple_string()
+    run_test_make_less_simple_string()
 
     # -------------------------------------------------------------------------
     # DONE 8. Uncomment the tests below before working _TODO_ 9.
     #   They launch annoying rg.RoseWindows on each run that you don't want
     #   until you get to _TODO_ 9 and _TODO_ 10.
     # -------------------------------------------------------------------------
-    # run_test_draw_shapes()
-    # run_test_rectangles_from_circles()
+    run_test_draw_shapes()
+    run_test_rectangles_from_circles()
 
 
 def run_test_make_simple_list():
